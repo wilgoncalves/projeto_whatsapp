8413a9c5ab3fb84d52512efc59a417521f93d4c3
Willian Gonçalves <122633904+wilgoncalves@users.noreply.github.com>
2026-04-06 08:54:54 -0300

    first commit

---
+++ app/src/main/java/com/williangoncalves/whatsapp/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.williangoncalves.whatsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void abrirTelaCadastro(View view) {
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+}
